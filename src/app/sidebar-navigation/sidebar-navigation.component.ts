60da8a800dd86d53a7fa3d4a55e8ad584d2124f1
David <sidebysidecc@gmail.com>
2023-04-21 23:37:56 -0700

    Fixed Sidebar-navigation toggle menu for mobile-view

--- src/app/sidebar-navigation/sidebar-navigation.component.ts
+++ src/app/sidebar-navigation/sidebar-navigation.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as bootstrap from 'bootstrap';
 @Component({
   selector: 'app-sidebar-navigation',
   templateUrl: './sidebar-navigation.component.html',
@@ -9,7 +9,31 @@ export class SidebarNavigationComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
 
+  ngOnInit(): void {
+    // Activate Bootstrap scrollspy on the main nav element
+    const sideNav = document.body.querySelector('#sideNav');
+    if (sideNav) {
+      new bootstrap.ScrollSpy(document.body, {
+        target: '#sideNav',
+        rootMargin: '0px 0px -40%',
+      });
+    }
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = Array.from(
+      document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    // @ts-ignore
+    responsiveNavItems.forEach((responsiveNavItem: HTMLElement) => {
+      responsiveNavItem.addEventListener('click', () => {
+        // @ts-ignore
+        if (window.getComputedStyle(navbarToggler).display !== 'none') {
+          // @ts-ignore
+          navbarToggler.click();
+        }
+      });
+    });
+  }
 }
