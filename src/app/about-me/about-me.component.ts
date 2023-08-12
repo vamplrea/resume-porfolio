e13cb9a076558f10ded77ca1c3e90387c5458b60
David <sidebysidecc@gmail.com>
2023-08-12 02:27:33 -0700

    Updated skills, work-experienced

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -12,4 +12,12 @@ export class AboutMeComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  downloadPdf(): void {
+    const pdfUrl = 'assets/DAVID HUYNH-resume.pdf'; // Update with the actual path to your PDF file
+    const anchor = document.createElement('a');
+    anchor.href = pdfUrl;
+    anchor.download = 'DAVID HUYNH-resume.pdf'; // Specify the desired file name for download
+    anchor.click();
+  }
+
 }
--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./skills.component.scss']
 })
 export class SkillsComponent implements OnInit {
-
+  TechnicalSkills :{ [key: string]: string } = {};
   constructor() { }
 
   ngOnInit(): void {
+    this.TechnicalSkills ={
+      'Web Development': "Angular, Spring framework, Spring Boot, HTML5, CSS3, SCSS, RxJs, Flutter",
+      'UI Frameworks': "Bootstrap, ngMaterial, Clarity, Servlet(JSP), Thymeleaf",
+      'Programing Languages': "JavaScript (ES5, ES6), TypeScript, Java (8), Python, C#",
+      'Design Tool': "Figma, Adobe Suite (Photoshop, Illustrator, Indesign)",
+      'Testing': "Karma and Jasmine",
+      'API Tool': "Postman, Mockoon",
+      'Databases': "MySQL, PostgreSQL, MS SharePoint, Firebase",
+      'Version Control': "Git, Github",
+      'Development methodologies': "Agile/Scrum",
+      'Project management tools': "JIRA, Trello",
+      'Operating System': "Windows, Mac, Linux",
+      'Language': "Proficient in English, Fluency in Vietnamese"
+    }
   }
-
+  getSkillKeys() {
+    return Object.keys(this.TechnicalSkills);
+  }
 }
